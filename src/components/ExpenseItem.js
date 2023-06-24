06d1729c86d54e7824ce6fbfdf4bf87f8cae78d2
fadega <fadega.araya@gmail.com>
2023-06-24 15:23:44 -0600

    state change handled

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,15 +2,34 @@ import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
+import { useState } from "react";
 
 // WAY 1. Props as arguments - one object of key value pairs
 function ExpenseItem(props) {
+
+  // change state of title
+  const [title, setTitle] = useState(props.title);
+
+  function changeTitleHandler() {
+    setTitle("Updated!");
+  }
+
+  // retain old state to toggle between two states
+  // const [title, setTitle] = useState(props.title);
+  // const alternateTitle = "Updated!";
+  
+  // function changeTitleHandler() {
+  //   setTitle(title === props.title ? alternateTitle : props.title);
+  // }
+  
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
+      {/* <button onClick={()=>setTitle("Updated Title")}>Change Title</button> */}
+      <button onClick={changeTitleHandler}>Change Title</button>
       </div>
     </Card>
   );
