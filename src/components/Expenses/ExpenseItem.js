25fdfac56cece82d33bbf8511c52d3f98406a0d0
fadega <fadega.araya@gmail.com>
2023-06-24 15:52:08 -0600

    restructing merged

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,8 @@
 import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-<<<<<<< HEAD:src/components/Expenses/ExpenseItem.js
 import Card from "../UI/Card";
-=======
-import Card from "./Card";
 import { useState } from "react";
->>>>>>> main:src/components/ExpenseItem.js
 
 // WAY 1. Props as arguments - one object of key value pairs
 function ExpenseItem(props) {
